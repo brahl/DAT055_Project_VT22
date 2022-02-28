b9ff30104b4597c9726300c1d9cbf8dbdae4f9ae
brahl <brahlsolutions@gmail.com>
2022-02-28 17:34:46 +0100

    Scraper, bare

--- src/Scraper.java
+++ src/Scraper.java
@@ -4,41 +4,50 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Scraper {
-    String[] year = new String[2000];
-    String[] point = new String[2000];
-    private String[][] res = new String[2000][2];
-
+    FavEducation fe = new FavEducation();
     public Scraper() throws IOException {
-        File tempFile = new File("tempFileS");
-        scrape(tempFile);
-
-
+        scrape();
     }
 
-    public void scrape(File tempFile) {
+    public void scrape() {
         Document document;
+        String s;
+        int i = 0;
         try {
-            //Get Document object after parsing the html from given url.
             document = Jsoup.connect("https://www.studera.nu/jamfor-utbildning/?asp=Chalmers+tekniska+h%c3%b6gskola&ast=DATATEKNIK%2c+CIVILINGENJ%c3%96R&q=datateknik&f=2%5bcth&e=&ce=e.uoh.cth.tkdat.49000.20222%2ce.uoh.cth.tidal.62000.20222&cv=").get();
-
             Elements table = document.getElementsByTag("table");
-
             for (Element e : table) {
-                writeToTemp(e.getElementsByClass("sr-only").toString(), tempFile);
+                writeToTemp(e.getElementsByClass("sr-only").toString());
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    public void writeToTemp(String s, File tempfile) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter(tempfile);
-        writer.println(s);
-        writer.close();
+    public FavEducation writeToTemp(String s) throws FileNotFoundException {
+        String result;
+        Scanner scanner = new Scanner(s);
+        while (scanner.hasNextLine()) {
+            result = scanner.nextLine();
+            if(result.contains("BI") && !result.contains("BII")) {
+                fe.pName = "Datateknik";
+                fe.uni = "Chalmers";
+                fe.credits = "180hp";
+                result = scanner.nextLine();
+                fe.admissionM1 = result.substring(59,64);
+                result = scanner.nextLine();
+                fe.admissionM2 = result.substring(59,64);
+                result = scanner.nextLine();
+                fe.admissionM3 = result.substring(59,64);
+                result = scanner.nextLine();
+                fe.admissionM4 = result.substring(59,64);
+                result = scanner.nextLine();
+                fe.admissionM5 = result.substring(59,64);
+            }
+        }
+        return fe;
     }
-
 }
