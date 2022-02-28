71dbd6cfb54c07cfe72787607405bcd33a3aa781
Viktors <viktor.rafstedt@hotmail.com>
2022-02-28 18:08:12 +0100

    Complete commit

--- src/App.java
+++ src/App.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         String user = "001";
-        //new StuartView(user);
+        new StuartView(user);
         new Scraper();
+
     }
 }
