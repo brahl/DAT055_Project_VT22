9c04640a7692b9a5a671357b21170444edf05a4d
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2022-03-01 12:46:06 +0100

    Merge of login with StuartView, and creation of tryLogin

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
+import javax.swing.*;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
+
+
+        //full app
         String user = "001";
-        new Login();
+        LogInPortal frame = new LogInPortal();
+        frame.setTitle("Stu.art");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 500, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        new App();
+
+        //uncomment for testing
         //new StuartView(user);
 
 
