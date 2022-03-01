7725f86c8398b78efdefa5b0376c9f5eb716cc6c
Viktors <viktor.rafstedt@hotmail.com>
2022-03-01 12:38:40 +0100

    Updated scraper to handle both BI, BII and HP admissiontypes

--- src/Scraper.java
+++ src/Scraper.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
 
 public class Scraper {
     FavEducation fe = new FavEducation();
-    public Scraper() throws IOException {
-        scrape();
+    public Scraper(String admissionType) throws IOException {
+        scrape(admissionType);
     }
 
-    public void scrape() {
+    public void scrape(String admissionType) {
         Document document;
         String s;
         int i = 0;
@@ -20,33 +20,74 @@ public class Scraper {
             document = Jsoup.connect("https://www.studera.nu/jamfor-utbildning/?asp=Chalmers+tekniska+h%c3%b6gskola&ast=DATATEKNIK%2c+CIVILINGENJ%c3%96R&q=datateknik&f=2%5bcth&e=&ce=e.uoh.cth.tkdat.49000.20222%2ce.uoh.cth.tidal.62000.20222&cv=").get();
             Elements table = document.getElementsByTag("table");
             for (Element e : table) {
-                writeToTemp(e.getElementsByClass("sr-only").toString());
+                writeToTemp(e.getElementsByClass("sr-only").toString(), admissionType);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public FavEducation writeToTemp(String s) throws FileNotFoundException {
+    public FavEducation writeToTemp(String s, String admissionType) throws FileNotFoundException {
         String result;
         Scanner scanner = new Scanner(s);
         System.out.println(s);
         while (scanner.hasNextLine()) {
             result = scanner.nextLine();
-            if(result.contains("BI") && !result.contains("BII")) {
-                fe.pName = "Datateknik";
-                fe.uni = "Chalmers";
-                fe.credits = "180hp";
-                result = scanner.nextLine();
-                fe.admissionM1 = result.substring(59,64);
-                result = scanner.nextLine();
-                fe.admissionM2 = result.substring(59,64);
-                result = scanner.nextLine();
-                fe.admissionM3 = result.substring(59,64);
-                result = scanner.nextLine();
-                fe.admissionM4 = result.substring(59,64);
-                result = scanner.nextLine();
-                fe.admissionM5 = result.substring(59,64);
+            //For admissionType == "BI" or "BII"
+            if(admissionType.equals("BI") || admissionType.equals("BII")) {
+                if (result.contains("(" + admissionType + ")")) {
+                    fe.pName = "Datateknik";
+                    fe.uni = "Chalmers";
+                    fe.credits = "180hp";
+                    result = scanner.nextLine();
+                    fe.admissionM1 = result.substring(59, 64);
+                    result = scanner.nextLine();
+                    fe.admissionM2 = result.substring(59, 64);
+                    result = scanner.nextLine();
+                    fe.admissionM3 = result.substring(59, 64);
+                    result = scanner.nextLine();
+                    fe.admissionM4 = result.substring(59, 64);
+                    result = scanner.nextLine();
+                    fe.admissionM5 = result.substring(59, 64);
+                }
+            }
+
+            if(admissionType.equals("HP")) {
+                if (result.contains("(" + admissionType + ")")) {
+                    fe.pName = "Datateknik";
+                    fe.uni = "Chalmers";
+                    fe.credits = "180hp";
+                    result = scanner.nextLine();
+                    if (result.contains("Lägst")) {
+                        fe.admissionM1 = result.substring(59, 64);
+                    } else {
+                        fe.admissionM1 = "No data exists";
+                    }
+                    result = scanner.nextLine();
+                    if (result.contains("Lägst")) {
+                        fe.admissionM2 = result.substring(59, 64);
+                    } else {
+                        fe.admissionM1 = "No data exists";
+                    }
+                    result = scanner.nextLine();
+                    if (result.contains("Lägst")) {
+                        fe.admissionM3 = result.substring(59, 64);
+                    } else {
+                        fe.admissionM1 = "No data exists";
+                    }
+                    result = scanner.nextLine();
+                    if (result.contains("Lägst")) {
+                        fe.admissionM4 = result.substring(59, 64);
+                    } else {
+                        fe.admissionM1 = "No data exists";
+                    }
+                    result = scanner.nextLine();
+                    if (result.contains("Lägst")) {
+                        fe.admissionM5 = result.substring(59, 64);
+                    } else {
+                        fe.admissionM1 = "No data exists";
+                    }
+                }
             }
         }
         return fe;
