b931da4b734786937cf95c7e3df6c8dabc82c26d
Erik Hillestad <68737617+erikhillestad@users.noreply.github.com>
2022-03-01 17:29:03 +0100

    Quote API added under Quotes class. Further implementation to view left.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         //full app
         String user = "001";
