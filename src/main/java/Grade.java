7cfa34f07ac11c16d2dfdcc7dd5ac7818aa79acb
Viktors <viktor.rafstedt@hotmail.com>
2022-03-04 14:39:06 +0100

    Javadoc added to Grade

--- src/main/java/Grade.java
+++ src/main/java/Grade.java
@@ -10,6 +10,9 @@ class Grade {
     private double grade;
     private String kurspoäng;
 
+    /**
+     * Constructs a default grades object
+     */
     public Grade() {
         this.kurs = "empty";
         this.lettergrade = "F";
@@ -17,7 +20,27 @@ class Grade {
         this.kurspoäng = "100";
     }
 
+    /**
+     * Constructs a grade object with specified values
+     * @param k The course name
+     * @param lg The grade (has to be either "A","B","C","D","E" or "F")
+     * @param kp The course credits
+     * @throws ArithmeticException if the grade is not as specified
+     * @throws ArithmeticException if the course credits is not numeric
+     */
     public Grade(String k, String lg, String kp) {
+        if(!lg.equals("A") || !lg.equals("B") || !lg.equals("C") || !lg.equals("D") || !lg.equals("E") || !lg.equals("F")){
+            JPanel panel = new JPanel();
+            JOptionPane.showMessageDialog(panel, "The grade must be either A,B,C,D,E or F in capital letter", "Warning", JOptionPane.WARNING_MESSAGE);
+            throw new ArithmeticException("Bad grade input");
+        }
+        try{
+            Integer.parseInt(kp);
+        }catch(NumberFormatException e){
+            JPanel panel = new JPanel();
+            JOptionPane.showMessageDialog(panel, "The course credits must be a positive number", "Warning", JOptionPane.WARNING_MESSAGE);
+            throw new ArithmeticException("Bad course credits input");
+        }
         this.kurs = k;
         this.lettergrade = lg;
         this.kurspoäng = kp;
@@ -41,15 +64,34 @@ class Grade {
         return this.grade;
     }
 
+    /**
+     * Getter for the coursename
+     * @return
+     */
     public String getkurs(){
         return this.kurs;
     }
+
+    /**
+     * Getter for the course grade
+     * @return
+     */
     public String getGrade(){
         return this.lettergrade;
     }
+
+    /**
+     * Getter for the coursepoints
+     * @return
+     */
     public String getKurspoäng(){
         return this.kurspoäng;
     }
+
+    /**
+     * Getter for the course grade as a double
+     * @return
+     */
     public Double getGradeInt() {
         return this.grade;
     }
